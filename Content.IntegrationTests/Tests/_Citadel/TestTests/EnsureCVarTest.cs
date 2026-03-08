c8bd31a3474b85fede6fcb4698899e47d62e8ea9
kaylie <moony@hellomouse.net>
2026-03-08 22:11:20 +0100

    better explainer.

--- Content.IntegrationTests/Tests/_Citadel/TestTests/EnsureCVarTest.cs
+++ Content.IntegrationTests/Tests/_Citadel/TestTests/EnsureCVarTest.cs
@@ -3,8 +3,7 @@ using Robust.Shared.Configuration;
 
 namespace Content.IntegrationTests.Tests._Citadel.TestTests;
 
-// Manually run using NUnit tooling.
-[Explicit]
+[Explicit("Manually run by NUnit tooling, only run this yourself if you're debugging.")]
 public sealed class EnsureCVarTest : GameTest
 {
     [SidedDependency(Side.Server)] private readonly IConfigurationManager _sCfgMan = default!;
