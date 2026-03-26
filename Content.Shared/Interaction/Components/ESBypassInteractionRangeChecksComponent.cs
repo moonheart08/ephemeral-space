1f77a96aeb034e8a5c54015792f92f56cdbd0d11
kaylie <moony@hellomouse.net>
2026-03-26 11:35:11 -0700

    not you too.

--- Content.Shared/Interaction/Components/ESBypassInteractionRangeChecksComponent.cs
+++ Content.Shared/Interaction/Components/ESBypassInteractionRangeChecksComponent.cs
@@ -1,7 +1,10 @@
-﻿namespace Content.Shared.Interaction.Components;
+﻿using Robust.Shared.GameStates;
+
+namespace Content.Shared.Interaction.Components;
 
 /// <summary>
 ///     Marks an entity as not caring about interaction limitations, like walls or not fitting criteria.
 ///     This is primarily used on admin ghosts.
 /// </summary>
+[RegisterComponent, NetworkedComponent]
 public sealed partial class ESBypassInteractionRangeChecksComponent : Component;
