b17156095d7a845ed2c479e9a69c3103ce40d31a
EmoGarbage404 <retron404@gmail.com>
2026-04-04 18:42:28 -0400

    COMMAND

--- Content.Server/_ES/ServerStatus/RerollHostnameCommand.cs
+++ Content.Server/_ES/ServerStatus/RerollHostnameCommand.cs
@@ -1,8 +1,11 @@
-﻿using Robust.Shared.Toolshed;
+﻿using Content.Server.Administration;
+using Content.Shared.Administration;
+using Robust.Shared.Toolshed;
 
 namespace Content.Server._ES.ServerStatus;
 
 [ToolshedCommand]
+[AdminCommand(AdminFlags.Fun)]
 public sealed class RerollHostnameCommand : ToolshedCommand
 {
     [Dependency] private readonly StatusManager _status = default!;
